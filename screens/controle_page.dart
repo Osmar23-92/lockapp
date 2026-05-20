45d855f24d13b1d2ee261658437f5baf5c86c9aa
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-05-20 15:35:24 -0300

    Add files via upload

---
+++ screens/controle_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ControlePage extends StatelessWidget {
+  const ControlePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
