5f92bc6d12b3a289973bd6c130dc815e1d8db427
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-05-25 20:51:44 -0300

    Add files via upload

--- services/user_data.dart
+++ services/user_data.dart
@@ -5,6 +5,12 @@ class UserData {
   static String emailCadastrado = "";
   static String senhaCadastrada = "";
   static String nomeUsuario = "";
+  static String fotoUsuario = "";
+  static String caminhoFoto = "";
+  static String nomePerfilSecundario = "";
+  static String fotoPerfilSecundario = "";
+
+  
 }
 
 
