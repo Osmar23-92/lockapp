f1f2a03b7d77e087b341b1fac36bb26c8b43f8f7
LauschSama <analulausch@gmail.com>
2026-05-27 16:38:21 -0300

    Add files via upload

---
+++ screens/editar_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditarPage extends StatelessWidget {
+  const EditarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
